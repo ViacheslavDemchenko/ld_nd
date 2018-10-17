027d5e29e07cc4ae0be630988809f48c1d2f6f37
Viacheslav Demchenko <demchenko_slava_@mail.ru>
2018-10-17 14:16:50 +0300

    Finished scripts

---
+++ js/index.js
@@ -0,0 +1,151 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
+
+	//СМЕЩЕНИЕ ФОНА В HEADER ПРИ ДВИЖЕНИИ КУРСОРА
+
+	var headerImgWrap = document.querySelector('.header__bg__wrap'),
+	    //Контейнер фоновой картинки
+	headerImg = document.querySelector('.header__bg__img'),
+	    //Фоновая картинка
+	width = window.screen.width,
+	    //Текущая ширина экрана
+	height = window.screen.height; //Текущая высота экрана
+
+	//Обработчик события для смещения фона в header при движении курсора мыши
+	headerImgWrap.addEventListener('mousemove', function (e) {
+
+		//Задаем коэффициент смещения фона относительно контейнера картинки
+		var moveX = (width / 2 - e.clientX) * 0.05,
+		    moveY = (height / 2 - e.clientY) * 0.05;
+
+		//Смещаем фоновую картинку по ширине и высоте на полученный коэффициент
+		headerImg.style.backgroundPositionX = moveX + 'px';
+		headerImg.style.backgroundPositionY = moveY + 'px';
+	});
+
+	//КОНТРОЛЬ ФОНА МЕНЮ (HEADER)
+
+	var header = document.querySelector('.header'); //Фон меню сайта
+
+	//Смена фона при прокрутке вниз и возвращении наверх
+	var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+	window.addEventListener('scroll', function () {
+		scrolled = window.pageYOffset || document.documentElement.scrollTop;
+		if (scrolled > 70) {
+			//Если прокрутка превышает 70px, фон становится светлым
+			header.classList.add('header--grey');
+		} else {
+			//Если прокрутка меньше 70px, фон становится прежним
+			header.classList.remove('header--grey');
+		}
+	});
+
+	//Проверка фона после перехагрузки страницы
+	function screenSizeCheck() {
+		if (scrolled > 70 || width < 993) {
+			header.classList.add('header--grey');
+		} else {
+			header.classList.remove('header--grey');
+		}
+	};
+	screenSizeCheck();
+
+	//ТАЙМЕР ОБРАТНОГО ОТСЧЕТА
+
+	//Функция получения времени
+	function getTimeRemaining(endtime) {
+		var t = Date.parse(endtime) - Date.parse(new Date()),
+		    seconds = Math.floor(t / 1000 % 60),
+		    minutes = Math.floor(t / 1000 / 60 % 60),
+		    hours = Math.floor(t / (1000 * 60 * 60) % 24),
+		    days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	};
+
+	function initializeClock(id, endtime) {
+		var clock = document.getElementById(id),
+		    daysSpan = clock.querySelector('.days'),
+		    hoursSpan = clock.querySelector('.hours'),
+		    minutesSpan = clock.querySelector('.minutes'),
+		    secondsSpan = clock.querySelector('.seconds');
+
+		function updateClock() {
+			var t = getTimeRemaining(endtime);
+
+			daysSpan.innerHTML = t.days;
+			hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+			minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+			secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+			if (t.total <= 0) {
+				clearInterval(timeinterval);
+			}
+		};
+		updateClock();
+
+		var timeinterval = setInterval(updateClock, 1000);
+	};
+
+	var end = new Date();
+	end.setHours(23, 59, 59, 999);
+
+	initializeClock('clockdiv', end);
+
+	//СМЕНА КАРТИНОК И ТЕКСТА В РАЗДЕЛЕ PRINCIPLES
+
+	var pictures = document.querySelectorAll('.principles-img'),
+	    //Псевдомассив картинок
+	articles = document.querySelectorAll('.principles__article'),
+	    //Псевдомассив статей
+	articleIndex = 0,
+	    //Текущая (активная) статья
+	pictureIndex = 0; //Текущая (активная) картинка
+
+	function imgTextChange() {
+
+		//Удаление активного класса у текущей статьи
+		articles[articleIndex].classList.remove('principles__article--active');
+		pictures[pictureIndex].classList.remove('principles-img--active');
+		articleIndex += 1; //Увеличение индекса статьи(выбор следующей статьи)
+		pictureIndex += 1; //Увеличение индекса картинки(выбор следующей картинки)
+
+		//Обнуление переменной при достижении последней статьи
+		if (articleIndex == articles.length) {
+			articleIndex = 0;
+		}
+		//Обнуление переменной при достижении последней картинки
+		if (pictureIndex == pictures.length) {
+			pictureIndex = 0;
+		}
+
+		//Присвоение активного класса следующей статье
+		articles[articleIndex].classList.add('principles__article--active');
+		pictures[pictureIndex].classList.add('principles-img--active');
+	};
+
+	var timeinterval = setInterval(imgTextChange, 5000);
+
+	// ВЫВОД ТЕКУЩЕГО ГОДА В ФУТЕРЕ
+
+	function getYear() {
+		var date = new Date(),
+		    currentYear = date.getFullYear(),
+		    yearWrite = document.querySelector('.year');
+
+		if (currentYear > 2018) {
+			yearWrite.innerHTML = '2018 - ' + currentYear;
+		} else {
+			yearWrite.innerHTML = currentYear;
+		}
+	};
+	getYear();
+});
---
+++ js/main_jq.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+//МОБИЛЬНОЕ МЕНЮ И ФОН МЕНЮ
+
+	var menuButton = $('#menu__button'),
+		menuMobile = $('.menu__list'),
+		menuLink = $('.menu__list__link'),
+		menuMobileOpen = 'menu__list--open';
+
+
+	function screenWidthCheck() {
+		if (menuButton.hasClass('active')) {
+	        menuButton.removeClass('active');
+	    } else {
+	        menuButton.addClass('active');
+	    }
+	};
+
+	menuButton.on('click', function(e) {
+		e.preventDefault();
+		menuMobile.toggleClass(menuMobileOpen);
+		screenWidthCheck();
+	});
+
+	menuLink.on('click', function() {
+		menuMobile.removeClass(menuMobileOpen);
+		screenWidthCheck();
+	});
+
+	$(window).resize(function() {
+		var w = $(window).width();
+			if (w > 768 ) {
+			    menuMobile.removeAttr('style');
+			    menuMobile.removeClass(menuMobileOpen);
+
+			    if (menuButton.hasClass('active')) {
+	       			menuButton.removeClass('active');
+	       		}
+			}
+	});
+
+//БЕГУЩИЕ СТРОКИ В HEADER
+
+	var typed = new Typed('#typed', {
+		    strings: ['"Радость — это то, что мы испытываем в процессе приближения к цели стать самим собой."', '"Изменение происходит, тогда, когда человек становится тем, кем он на самом деле является, а не тогда, когда человек предпринимает попытки стать тем, кем он не является."'],
+		    typeSpeed: 50,
+		    backDelay: 1000,
+		    loop: true
+  		}); 
+
+//ПЛАВНАЯ ПРОКРУТКА ЭКРАНА
+
+  	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({highlightSelector:"nav a"
+	}); 
+
+});
